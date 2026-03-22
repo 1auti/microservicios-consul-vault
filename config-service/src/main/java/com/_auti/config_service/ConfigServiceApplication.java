4c9c34e792f00d733221c60972042565be6d004d
1auti <lautacenizo@gmail.com>
2026-03-21 21:36:28 -0300

    se agrego la configuracuib de los repos de config

--- config-service/src/main/java/com/_auti/config_service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/_auti/config_service/ConfigServiceApplication.java
@@ -2,8 +2,13 @@ package com._auti.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // Le dice a Spring Boot que esta aplicación es un servidor de configuración
+@EnableDiscoveryClient // Permite que esta aplicación se registre en el servicio de descubrimiento
+						// (Eureka, Cosul, etc.)
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
