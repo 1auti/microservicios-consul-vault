73059e15e5d66ff4347a4e51896ace917d19f8c0
1auti <lautacenizo@gmail.com>
2026-03-22 12:27:58 -0300

    se unico pom en padre y hijo

---
+++ eureka-server/src/test/java/com/lautaro/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lautaro.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
